f638ebe2ba1d778209ba85de78834cbbf3670cfc
Chicken-Ninja <eko1@stuy.edu>
2018-02-06 01:33:25 -0500

    Finished it + tested

---
+++ 01Blah/Recursion.java
@@ -0,0 +1,22 @@
+public class Recursion {
+    public int factorial(int n){
+	if (n < 0) {throw new IllegalArgumentException();} 
+	if (n == 0) {return 1;}		       
+	return n * factorial(n - 1); }
+
+    public int fib(int n) {
+	if (n < 0) {throw new IllegalArgumentException();}
+	if (n == 0) {return 0;} 
+	if (n == 1) {return 1;} 
+	return fib(n - 1) + fib(n - 2); }
+    public double sqrt (double n) {
+	if(n < 0) {throw new IllegalArgumentException();}
+	return sqrthelp(n , 1);}
+    public double sqrthelp( double n , double guess){
+	if(n < 0) {throw new IllegalArgumentException(); }
+	if (guess * guess - n < 0.01 && guess * guess - n > -0.01)  {return guess;} 
+	else{return sqrthelp(n , ((n / guess) + guess) / 2);}
+    }
+  
+
+}
