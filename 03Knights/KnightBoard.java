c9261c72ec97ff1b482c957b3aca6bee8788b1ff
Chicken-Ninja <eko1@stuy.edu>
2018-02-28 01:41:08 -0500

    some solve thing

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -1,7 +1,7 @@
 public class KnightBoard {
     private int[][] board; 
-    public int[][] moves = [ [
-
+    public int[][] moves;
+    public int counter = 0; 
 
     public KnightBoard (int rows , int cols) {
 
@@ -16,7 +16,25 @@ public class KnightBoard {
 			board[stepper][counter] = 0;
 		    }
 	    }
+	
+
+
+
+
+
+
+
+	int[][] moves = new int[2][8];
+	int[] x = {1 , 2 , -1 , -2 , 1 , 2 , -1 , -2};
+	int[] y = {2 , 1 , 2 , 1 , -2 , -1, -2 , -1};
+	moves[0] = x; 
+	moves[1] = y;
+
     }
+    
+
+
+
     public String toString() {
 	String dump = ""; 
 	for(int counter = 0; counter < board[0].length - 1; counter ++ ) 
@@ -33,9 +51,24 @@ public class KnightBoard {
 	    }
 	return dump;
     }
-    public boolean solve(int startingRow , int startingCol) {} 
+
+
+    public boolean solve(int startingRow , int startingCol) {
+	//If the board filled 
+	int isFilled = true; 
+	for(int counter = 0; counter < board.length - 1; counter ++ ) 
+	    {for (int stepper = 0; stepper < board.length - 1; stepper ++) 
+		    {if(board[stepper][counter] == 0)  
+			    {isFilled = false;}
+		    }
+	    }
+	if(isFilled = true) {return true;}
+	
+	for(int counter = 0; counter < 
 		    
-  
+	
+		    
+
 }
     
 
