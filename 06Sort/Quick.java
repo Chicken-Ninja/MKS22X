bbf5b616ff2ae2edce7749921c812c45bdd6318e
Chicken-Ninja <eko1@stuy.edu>
2018-03-18 18:40:11 -0400

    Got my quickSElect to work

--- 06Sort/Quick.java
+++ 06Sort/Quick.java
@@ -30,11 +30,11 @@ public class Quick{
 	int gt = end; 
 	int pivot =  rand.nextInt(end-start+1) + start;
 	int check = nums[pivot];
-	System.out.println(nums[pivot]);
+	//	System.out.println(nums[pivot]);
 	while(i <= gt) 
 	    {
-		System.out.println(toString(nums));
-		System.out.println(i);
+		//	System.out.println(toString(nums));
+		//System.out.println(i);
 		if(nums[i] == check) 
 		    {i++;}
 	        else if(nums[i] > check) 
@@ -49,16 +49,66 @@ public class Quick{
 		}
 	    }
     
-    return 0;
+	return gt;} 
+
+
+    public static int quickSelect(int nums[], int k) 
+    {
+	int start = 0;
+	int end = nums.length - 1;
+	int v = 0; 
+	v = partition(nums , start, end); 
+	while(v != k) 
+	    { 
+		if(v < k ) 
+		    {
+			start = v; 
+		    }
+		if(v > k) 
+		    {
+			end = v; 
+		    }
+		
+		v= partition(nums, start , end);
+	    }
+	return nums[v];
     }
+
+    public static void quickSort (int[]  nums) 
+    {
+	 quickHelp( nums , 0 , nums.length - 1); 
+    }
+
+    public static void quickHelp(int[] nums, int start, int  end) 
+    {
+	int v;
+	v = partition(nums , start , end) ;
+	while(start < end) 
+	    {
+		quickHelp(nums, 0 , v);
+		quickHelp(nums, v , end);
+	    }
+       
+    }
+	
+	
+    
+
 
     public static void main (String[] args) 
     {
 	int[]a = new int[] {9999, 0 , 0 , 4 , 3, 99999 , 12 , 400 ,30};
 	Quick.partition(a , 0 , 8);
 	System.out.println(toString(a));
+	System.out.println(" " );
+	System.out.println(Quick.quickSelect(a , 4));
+	Quick.quickSort(a);
+	System.out.println(toString(a));
+	
+
     }
 }
+
 
 			
 	
