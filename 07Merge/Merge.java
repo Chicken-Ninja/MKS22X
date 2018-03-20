4b63b0f7203426f21031a5751d4e67a5e267565f
Chicken-Ninja <eko1@stuy.edu>
2018-03-20 14:38:10 -0400

    Added toString and the mergeSort(not the msort)

---
+++ 07Merge/Merge.java
@@ -0,0 +1,26 @@
+public class Merge{
+
+    
+    public static String toString(int[] r)
+    {
+	String dump = "[ "
+	for(int counter = 0; counter < r.length; counter++ )
+	    {
+		dump = dump + r[counter];
+		dump = dump + " ";
+	    }
+	return dump; 
+    }
+
+    public static void mergesort(int[] data)
+    {
+	int[]temp = new int[data.length];
+	msort(data, temp , 0 , data.length -1 );
+    }
+
+
+    public static void msort(int[] data, int[] temp , int lo, int hi)
+    {
+    }
+
+}
