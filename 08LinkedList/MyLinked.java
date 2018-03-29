68990385028141fc94cebec15f2db12a972d6b7e
Chicken-Ninja <eko1@stuy.edu>
2018-03-29 00:47:03 -0400

    Added the set and get methods as well as the toString

--- 08LinkedList/MyLinked.java
+++ 08LinkedList/MyLinked.java
@@ -1,54 +1,78 @@
 public class MyLinked {
-       Node first;
-       Node last;
-       int length; 
-
-       public MyLinkedList()
-       {
-       }
-
-       public boolean add(int value)
-       {
-       }
-       
-       private int size()
-       {
+    Node first;
+    Node last;
+    int length; 
+    
+    public MyLinkedList()
+    {
+    }
+    
+    public boolean add(int value)
+    {
+    }
+    
+    private int size()
+    {
 	return length; 
-       }
-
-       private String toString()
-       {
-       }
-
-       private int get(int index)
-       {int value = 0; 
-	if(index > length) {throw new IllegalArgumentException();}
-	for (int counter = 0; counter <= index; counter++)
+    }
+    
+    private String toString()
+    {
+	String dump = "";
+	Node temp = start; 
+	while(Node.getNext().getValue()!= null) 
 	    {
-		if(index == counter) {
-			 return value;
-			 }
-	        else{value = Node.getNext().getValue();}
-		}
-			 
-			 
+		temp = Node.getNext();
+		dump = dump + temp.getValue();
+		dump += " "; 
+	    }
+	return dump; 
+    }
+    
+    private int get(int index)
+    {
+	Node temp = start; 
+	for(int counter = 0; counter <= index; counter++ ) 
+	    {
+		temp = temp.getNext();
+	    }
+	return temp.getValue();
+    }
+    
 			
-	
-       }
+    
 
-       private int set(int index, int newValue)
-       {
-       }
-
-       
-
-}
+    
+    private int set(int index, int newValue)
+    {
+	Node temp = start;
+	for(int counter = 0; counter <= index; counter++ )
+	    {
+		
+		if(counter == index) 
+		    {
+			return temp.getValue();
+		    }
+		if(Node.getNext().getValue() != null) 
+		    {
+			temp = Node.getNext();
+			
+		    }
+		
+		
+		
+	    }
+    
+    
+    
+    }
 
 public class Node {
     Node next;
     Node Prev; 
     int data;
     
+
     public Node (Node a, Node b , int c) {
 	next = a; 
 	Prev = b; 
