8d19cbbcc2538fef79a58047073642dbfe2adcc8
Chicken-Ninja <eko1@stuy.edu>
2018-04-17 14:49:24 -0400

    Added slightly more stuff

--- 09Eval/eval.java
+++ 09Eval/eval.java
@@ -1,3 +1,15 @@
 public class eval
 {
+    //String data;
+    
+    int value = 0;
+    String[] data;
+
+    public eval(String datum)
+    {
+	String temp  = "" 
+	    for(int counter = 0; counter < String.length(); counter ++)
+		{
+		    
+    
     
