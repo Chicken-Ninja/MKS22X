e319120b48ebc75f441d6210e9b142581f0ea189
Chicken-Ninja <eko1@stuy.edu>
2018-04-18 03:29:59 -0400

    Whoops

--- 09Eval/eval.java
+++ 09Eval/eval.java
@@ -3,10 +3,10 @@ public class eval
     //String data;
     
     int value = 0;
-    String[] data;
 
-    public eval(String datum)
+    public static double eval(String datum)
     {
+	STring values = 
 	String temp  = "" 
 	    for(int counter = 0; counter < String.length(); counter ++)
 		{
