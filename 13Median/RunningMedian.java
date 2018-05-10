6a6cbaa61dc4c1db11e1708dc9e591cbe84d691e
Chicken-Ninja <eko1@stuy.edu>
2018-05-10 14:50:58 -0400

    Tried to make stuff work, median alsmot done

---
+++ 13Median/RunningMedian.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+
+
+
+public class RunningMedian
+{
+    MyHeap<Double> min;
+    MyHeap<Double> max;
+    double median;
+
+    public RunningMedian()
+    {
+	MyHeap<Double> min = new MyHeap<>(false);
+	MyHeap<Double> max = new MyHeap<>(true);
+	median = 0.0; 
+    }
+
+    public void add(Double value)
+    {
+	//	System.out.println(max.size());
+	//      System.out.println(min.size());
+
+	if(max.size() - min.size() > 1)
+	    {
+		for(int counter = max.size() - min.size(); counter > 0; counter-- )
+		    {
+			Double temp = max.remove() ;
+			min.add(temp);
+		    }
+	    }
+	else if(min.size() - max.size() > 1)
+	    {
+		for(int counter = max.size() - min.size(); counter > 0; counter-- )
+		    {
+			Double temp = min.remove();
+			max.add(temp);
+		    }
+	    }
+    		
+	if(value >= median)
+	    {
+		min.add(value);
+	    }
+	if(value < median)
+	    {
+		max.add(value);
+	    }
+	
+	
+	if(max.size() -  min.size() == 1 )
+	    {
+		median = max.peek();
+	    }
+	else if(max.size() - min.size() == 1)
+	    {
+		median = min.peek() ;
+	    }
+	else
+	    {
+		median = (max.peek() + min.peek()) / 2;
+	    }
+    }
+
+    public String toString()
+    {
+	String dump = "";
+	dump += min.toString();
+	dump += max.toString();
+	return dump; 
+	
+    }
+
+    
+    public int size()
+    {
+	return max.size() + min.size();
+    }
+    
+
+
+    
+
+    public Double getMedian()
+    {
+	if(max.size() + min.size() == 0)
+	    {
+		throw new NoSuchElementException();
+	    }
+	
+	return median;
+    }
+
+    public static void main(String[] args)
+    {
+	RunningMedian a = new RunningMedian();
+	a.add(10.0);
+	System.out.println(a.toString());
+	a.add(12.0);
+	System.out.println(a.getMedian()) ;
+    }
+			   
+
+
+
+    
+}
+    
+    
+	   
+    
+    
+	
---
+++ 14Frontier/MazeSolver.java
@@ -0,0 +1,11 @@
+public class MazeSolver
+{
+    private Maze maze;
+    private Frontier frontier;
+
+    public MazeSolver(String mazeText)
+    {
+
+    }
+
+    public 
