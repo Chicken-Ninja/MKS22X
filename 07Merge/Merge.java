1965e3455943e7c0ddcf2c92f64c8cfc2395b070
Chicken-Ninja <eko1@stuy.edu>
2018-06-22 00:14:34 -0400

    NOTHING WORKS

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -12,29 +12,6 @@ public class Merge{
 	return dump; 
     }
 
-    public static void merge(int[]data ,int[]temp , int lo , int mid , int hi)
-    {
-        int first = lo;
-	int second = mid + 1; 
-	for(int counter = lo; counter < = hi; i++)
-	    {
-		if(firstI > mid) 
-		    {
-			data[counter] = temp[second];
-			second++;
-		    }
-		else if(second > hi)
-		    {
-			data[counter] = temp[first];
-			first ++;
-		    }
-		else if(temp[first] < temp[first] ) 
-		    {
-			data[counter] = temp[second];
-			second ++;
-		    }
-	    }
-    }
 
 
      private static void swap(int[]data,int a, int b){
@@ -52,19 +29,23 @@ public class Merge{
     public static void mergesort(int[] data)
     {
 	int[]temp = new int[data.length];
+	for(int counter = 0; counter <= data.length - 1; counter ++ ) 
+	    {
+		temp[counter] = data[counter];
+	    }
 	msort(data, temp , 0 , data.length -1 );
     }
 
-    public static void Insertion(int[] data, in start , int end)
+    public static void Insertion(int[] data, int start , int end)
     {
 	for(int counter = start + 1; counter <= end; counter ++ ) 
 	    {
 		int place = counter; 
 		for(int stepper = counter - 1; stepper >= start; stepper -- ) 
 		    {
-			if(data[stepper] > data[counter])
+			if(data[stepper] > data[place])
 			    {
-				swap(data , counter , place );
+				swap(data , stepper , place );
 				place = counter;
 			    }
 		    }
@@ -72,27 +53,80 @@ public class Merge{
     }
 									     
 							    
-							    
-
-
-
+    public static void merge(int[] data,int[] temp,int lo,int mid, int hi){
+	int first = lo;
+	int second = mid + 1;
+	for(int counter = lo; counter <= hi; counter++)
+	    {
+		if (first > mid)
+		    {
+			temp[first] = data[second];
+			second++;
+		    }
+		else if (second > hi)
+		    {
+			temp[counter] = data[first];
+			first++;
+		    }
+		else{
+		    if (data[first] < data[second])
+			{
+			    temp[counter] = data[first];
+			    first++;
+			}
+		    else
+			{
+			temp[counter] = data[second];
+			second++;
+			}
+		}
+	    }
+    }				    
+    
+    
+    
 
 
     public static void msort(int[] data, int[] temp , int lo, int hi)
     {
-	if(hi < lo  <= 25) 
+	if(hi - lo <= 25) 
 	    {
-		Insertion(data ,lo hi );
+		Insertion(data ,lo, hi );
+		return;
 	    }
-	for(int counter = lo ; counter < hi; counter ++ ) 
+	else if(hi - lo <= 1) 
 	    {
-		temp[counter] = data[counter ]; 
+		return;
+	    }
+	for(int counter = lo; counter <= hi ; counter++ )
+	    {
+		temp[counter] = data[counter];
 	    }
 	int mid = (lo + hi) / 2 ;
 	msort(temp, data, lo , mid );
 	msort(temp, data, mid + 1 , hi );
 	merge(data , temp, lo, mid , hi);
     }
+
+
+
+
+
+   public static void main(String [] args) 
+	{
+	    int[] a = {1 , 5 ,3 , 4 , 8 , 6, 7, 8,9};
+	    Merge.mergesort(a);
+	    String b = "";
+	    for(int counter = 0; counter < a.length; counter++ )
+		{
+		    b += a[counter];
+		    b += " ";
+		}
+	    System.out.println(b);
+	}
+		 
+    
+
 }
 	      
 		
